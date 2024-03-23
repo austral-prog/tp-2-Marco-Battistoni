0808e698368cf5ce0d7e24d795e27802924596d0
Marco-Battistoni <162721472+Marco-Battistoni@users.noreply.github.com>
2024-03-23 18:10:28 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    complete_name = f"{first_name} {last_name}"
+
+    print(complete_name.lower())
+    print(complete_name.title())
+    print(complete_name.upper())
+    print(f"\t{complete_name.lower()}")
